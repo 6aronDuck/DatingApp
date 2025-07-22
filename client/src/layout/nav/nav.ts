6eeb54f0b68fbbbd2572920dd2e06c29e35c5c3a
6aronDuck <nahorniak2022zx@gmail.com>
2025-07-22 14:39:01 -0500

    -added login

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
-import { inject } from '@angular/core';
+import { inject,signal } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -12,12 +12,22 @@ import { inject } from '@angular/core';
 export class Nav {
   private accountService = inject(AccountService);
   protected creds: any = {};
+  protected loggedIn = signal(false);
 
   login()
   {
     this.accountService.login(this.creds).subscribe({
-      next: result => console.log(result),
+      next: result => {
+        this.loggedIn.set(true);
+        this.creds = {};
+        console.log(result)
+      },
       error: error => alert(error.message)
     })
+  }
+
+  logout()
+  {
+    this.loggedIn.set(false);
   }
 }
