295881de7681145b73ef751c3ae01ad577864812
6aronDuck <nahorniak2022zx@gmail.com>
2025-07-31 14:43:00 -0500

    -added registration from the client

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -12,16 +12,30 @@ export class AccountService {
 
   baseUrl = 'https://localhost:5001/api/';
 
+  register(creds: LoginCreds) {
+    return this.http.post<User>(this.baseUrl + 'account/register', creds).pipe(
+      tap(user => {
+        if(user){
+          this.setCurrentUser(user);
+        }
+      })
+    );
+  }
+
   login(creds: LoginCreds)
   {
     return this.http.post<User>(this.baseUrl + 'account/login', creds).pipe(
       tap(user => {
         if(user){
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUser.set(user);
+          this.setCurrentUser(user);
         }
       })
     );
+  }
+
+  setCurrentUser(user: User) {
+    localStorage.setItem('user', JSON.stringify(user));
+    this.currentUser.set(user);
   }
 
   logout(){
--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,6 +1,7 @@
-import { Component, input, output } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RegisterCreds, User } from '../../../types/user';
+import { AccountService } from '../../../core/services/account-service';
 
 @Component({
   selector: 'app-register',
@@ -9,12 +10,21 @@ import { RegisterCreds, User } from '../../../types/user';
   styleUrl: './register.css'
 })
 export class Register {
-  membersFromHome = input.required<User[]>();
+  private accountService = inject(AccountService);
   cancelRegister = output<boolean>();
   protected creds = {} as RegisterCreds;
 
   register() {
     console.log('Registering user with credentials:', this.creds);
+    this.accountService.register(this.creds).subscribe({
+      next: (user) => {
+        console.log('User registered successfully:', user);
+        this.cancelRegister.emit(false);
+      },
+      error: (error) => {
+        console.error('Error registering user:', error);
+      }
+    });
     // Here you would typically call a service to handle the registration logic
   }
 
